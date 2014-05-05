9774131dcab0aadf0cc8a378f3f3267b6ce664f1
John Willman <johnpwillman@gmail.com>
2014-05-04 22:35:37 -0700

    Add FlyweightFontFactory and add test for it in TestFlyweight.java

---
+++ src/flyweight/FlyweightFontFactory.java
@@ -0,0 +1,31 @@
+package flyweight;
+
+import java.awt.Font;
+import java.util.Hashtable;
+
+public class FlyweightFontFactory {
+	
+	private Hashtable<String, Font> fonts;
+	
+	private FlyweightFontFactory() {}
+	
+	private static class SingletonHolder {
+		private final static FlyweightFontFactory INSTANCE = new FlyweightFontFactory();
+	}
+	
+	public static FlyweightFontFactory instance() {
+		return SingletonHolder.INSTANCE;
+	}
+	
+	public Font getFlyweight(String name, int style, int size) {
+		
+		String fontToRetrieve = name + "/" + Integer.toString(style) + "/" + Integer.toString(size);
+		
+		if (!fonts.contains(fontToRetrieve)) {
+			fonts.put(fontToRetrieve, new Font(name, style, size));
+		}
+		
+		return fonts.get(fontToRetrieve);
+	}
+	
+}
--- test/flyweight/TestFlyweight.java
+++ test/flyweight/TestFlyweight.java
@@ -1,7 +1,8 @@
 package flyweight;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+
+import java.awt.Font;
 
 import org.junit.Test;
 
@@ -28,8 +29,29 @@ public class TestFlyweight {
 				"Chars should be the same",
 				firstChar.equals(secondChar)
 				);
+	}
+
+	@Test
+	public void testFontFlyweight() {
 		
-		fail("Not yet implemented");
+		FlyweightFontFactory fff = FlyweightFontFactory.instance();
+		
+		Font firstFont, secondFont;
+
+		firstFont = fff.getFlyweight("Times New Roman", Font.BOLD, 12);
+		
+		try {
+			wait(10);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		secondFont = fff.getFlyweight("Times New Roman", Font.BOLD, 12);
+		
+		assertTrue(
+				"Fonts should be the same",
+				firstFont.equals(secondFont)
+				);
 	}
 
 }
